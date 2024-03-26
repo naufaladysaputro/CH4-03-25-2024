2acc1f6016a02010f5ace1b997acc36c37f3055b
naufaladysaputro <naufaladysaputro@gmail.com>
2024-03-27 00:50:30 +0700

    first commit

---
+++ controllers/customerController.js
@@ -0,0 +1,141 @@
+const fs = require("fs");
+const Customer = require("./../models/customerModel");
+
+
+
+const getCustomers = async (req, res, next) => {
+  try {
+    const queryObject = { ...req.query};
+    const excludedColumn = ["page", "sort", "limit", "fields"];
+    excludedColumn.forEach((el) => delete queryObject[el] );
+
+    console.log(req.query, queryObject);
+
+    const customers = await Customer.find(req.query);
+      // {
+      // active: true,
+    //   role: req.query.role,
+    //   active: req.query.active,
+    // });
+
+  res.status(200).json({
+    status: "success",
+    totalData: customers.length,
+    requestAt: req.requestTime,
+    data: {
+      customers,
+    },
+  });
+
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    })
+  }
+
+  
+};
+
+const getCustomerById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+  // menggunakan array method utk membantu menemukan spesifik data
+  const customer = customers.find((cust) => cust._id === id);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      customer,
+    },
+  });
+
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+  
+};
+
+const updateCustomer = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const customer = await Customer.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+    message: "berhasil update data",
+    data: {
+      customer,
+    },
+  });
+
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+
+    });
+  }
+};
+
+  
+const deleteCustomer = async (req, res) => {
+  try {
+  const id = req.params.id;
+
+  await Customer.findByIdAndDelete(id);
+
+      res.status(200).json({
+        status: "success",
+        message: "berhasil delete data",
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: "fail",
+        message: err.message,
+      });
+    }
+};
+
+// const createCustomer = async (req, res) => {
+  // console.log(req.body);
+
+  // const newCustomer = req.body;
+
+  // customers.push(newCustomer);
+
+  // nambah data
+  const createCustomer = async (req, res) => {
+    try {
+      const newCustomer = await Customer.create(req.body);
+  
+      res.status(201).json({
+        status: "success",
+        data: {
+          customer: newCustomer,
+        },
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: "fail",
+        message: err.message,
+      });
+    }
+  };
+
+
+module.exports = {
+  getCustomers,
+  getCustomerById,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer,
+};
